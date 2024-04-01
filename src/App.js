46b91273af0063dedb25a35f704980aea276c6a1
Ashutosh kushire <ashutoshkushire97@gmail.com>
2024-04-01 22:35:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
+
 export default function App() {
   const [WeatherData, setWeatherData] = useState(null);
   const [searchText, setSearchText] = useState("");
@@ -8,16 +10,12 @@ export default function App() {
   const performAPICall = async (text) => {
     setIsLoading(true);
     try {
-      let response = await fetch(
+      let response = await axios.get(
         `https://api.weatherapi.com/v1/current.json?key=3282328a03ac48ae92022337232809&q=${text}&aqi=no`
       );
-      if (!response.ok) {
-        throw new Error("Failed to fetch weather data");
-      }
-      let data = await response.json();
-      setWeatherData(data);
+      setWeatherData(response.data);
     } catch (error) {
-      alert(error.message);
+      alert("Failed to fetch weather data");
     } finally {
       setIsLoading(false);
     }
@@ -44,27 +42,30 @@ export default function App() {
         />
         <button onClick={handleSearch}>Search</button>
       </div>
-      <p>{isLoading ? "Loading data…" : ""}</p>
-      {WeatherData && (
-        <div className="weather-cards">
-          <div className="weather-card">
-            <h2>Temperature </h2>
-            <p>{WeatherData.current.temp_c}°C</p>
+      <>
+        {isLoading ? (
+          <p>Loading data…</p>
+        ) : WeatherData ? (
+          <div className="weather-cards">
+            <div className="weather-card">
+              <h2>Temperature </h2>
+              <p>{WeatherData.current.temp_c}°C</p>
+            </div>
+            <div className="weather-card">
+              <h2>Humidity</h2>
+              <p>{WeatherData.current.humidity}%</p>
+            </div>
+            <div className="weather-card">
+              <h2>Condition</h2>
+              <p>{WeatherData.current.condition.text}</p>
+            </div>
+            <div className="weather-card">
+              <h2>Wind Speed</h2>
+              <p>{WeatherData.current.wind_kph} kph</p>
+            </div>
           </div>
-          <div className="weather-card">
-            <h2>Humidity</h2>
-            <p>{WeatherData.current.humidity}%</p>
-          </div>
-          <div className="weather-card">
-            <h2>Condition</h2>
-            <p>{WeatherData.current.condition.text}</p>
-          </div>
-          <div className="weather-card">
-            <h2>Wind Speed</h2>
-            <p>{WeatherData.current.wind_kph} kph</p>
-          </div>
-        </div>
-      )}
+        ) : null}
+      </>
     </div>
   );
 }
