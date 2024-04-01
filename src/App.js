50a3c98ec7a926e614866e6399b14689b138b44b
Ashutosh kushire <ashutoshkushire97@gmail.com>
2024-04-01 22:11:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+export default function App() {
+  const [WeatherData, setWeatherData] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-function App() {
+  const performAPICall = async (text) => {
+    setIsLoading(true);
+    try {
+      let response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=3282328a03ac48ae92022337232809&q=${text}&aqi=no`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch weather data");
+      }
+      let data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSearch = () => {
+    if (searchText !== "") {
+      performAPICall(searchText);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="heading">
+        <h1>Weather App</h1>
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Enter location"
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <p>{isLoading ? "Loading data…" : ""}</p>
+      {WeatherData && (
+        <div className="weather-cards">
+          <div className="weather-card">
+            <h2>Temperature </h2>
+            <p>{WeatherData.current.temp_c}°C</p>
+          </div>
+          <div className="weather-card">
+            <h2>Humidity</h2>
+            <p>{WeatherData.current.humidity}%</p>
+          </div>
+          <div className="weather-card">
+            <h2>Condition</h2>
+            <p>{WeatherData.current.condition.text}</p>
+          </div>
+          <div className="weather-card">
+            <h2>Wind Speed</h2>
+            <p>{WeatherData.current.wind_kph} kph</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
